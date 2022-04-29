634c9a0c5644356fd6741b8cde12e128401b8d7b
Rien Boydens <rien.boydens@gmail.com>
2022-04-29 22:53:10 +0200

    Fixed var exports in subMenus.js

--- subMenus.js
+++ subMenus.js
@@ -489,7 +489,7 @@ class LocationsMenu extends HiddenSubMenuMenuItemBase{
 });
 
 
-let StackerBase = GObject.registerClass(
+var StackerBase = GObject.registerClass(
 {
 },
 /**
@@ -617,7 +617,7 @@ class StackerBase extends GObject.Object{
 /**
  * Class that implements faved server as a GUI menu item
  */
-let FavedServerItem = GObject.registerClass(
+var FavedServerItem = GObject.registerClass(
   {
     Signals: {
       'delete-fav': {
@@ -692,7 +692,7 @@ class FavedServerItem extends PopupMenu.PopupBaseMenuItem{
 /** Regisering this item as a GObject in order
  *  to use signals via the 'emit' method
  */
-let FavoriteStacker = GObject.registerClass(
+var FavoriteStacker = GObject.registerClass(
 {
   Signals: {
     'server-fav-connect': {
@@ -978,7 +978,7 @@ class RecentLocationItem extends PopupMenu.PopupBaseMenuItem{
   }
 });
 
-let RecentLocationStacker = GObject.registerClass(
+var RecentLocationStacker = GObject.registerClass(
 {
   Signals: {
     'location-connect': {
@@ -2104,7 +2104,7 @@ class OptionsSubDNSItem extends PopupMenu.PopupBaseMenuItem {
 /** Regisering this item as a GObject in order
  *  to use signals via the 'emit' method
  */
-let OptionsSubDNSItemContainer = GObject.registerClass(
+var OptionsSubDNSItemContainer = GObject.registerClass(
 {
   Signals: {
     'new-DNS-config': {
@@ -2256,7 +2256,7 @@ class OptionsSubDNSItemContainer extends GObject.Object{
  *  different options offered by the CLI tool.
  */
 
-let OptionsSubMenu = GObject.registerClass(
+var OptionsSubMenu = GObject.registerClass(
 class OptionsSubMenu extends HiddenSubMenuMenuItemBase{
   /** Constructor
    *  @method
@@ -2452,7 +2452,7 @@ class OptionsSubMenu extends HiddenSubMenuMenuItemBase{
 /** class that implements a inforative message as a gui
  * menu item
  */
-let MessageItem = GObject.registerClass(
+var MessageItem = GObject.registerClass(
 class MessageItem extends PopupMenu.PopupBaseMenuItem{
   /**
    * constructor
@@ -2511,7 +2511,7 @@ class MessageItem extends PopupMenu.PopupBaseMenuItem{
 /** Class that implements the menu item that shows a message when
  *  expected given version doesn't match actual given version
  */
-let VersionChecker = GObject.registerClass(
+var VersionChecker = GObject.registerClass(
 class VersionChecker extends MessageItem{
   /** Enumerator for the possible result of versions comparison
    *  @readonly
